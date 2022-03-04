162d7ea08d7bdb66319e0f2d141f2cad8e069783
caesar <joaovictorcsatu@gmail.com>
2022-03-04 08:12:51 -0300

    Added JWT Secret

--- controllers.js
+++ controllers.js
@@ -1,9 +1,16 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
     console.log("Provide missing input");
   }
+  //PARAMS AND JWT SECRET STRING
+  const token = jwt.sign({username,password},process.env.JWT.SECRET)
 };
+
+
 
 module.exports = { login };
