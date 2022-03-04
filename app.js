217a6ad279017fab925a579929722d14dd9e85ad
caesar <joaovictorcsatu@gmail.com>
2022-03-04 08:30:23 -0300

    Added an outpost for POST testing

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const { errorHandler } = require("./auxiliary_files/errorHandler");
 const express = require("express");
 const app = express();
+const port = 5000;
 
 app.use(errorHandler);
 
 app.use(express.json());
 
 app.use("/app", router);
+
+app.listen(port, () => {
+    console.log(`Port ${port} is listening...`);
+});
--- controllers.js
+++ controllers.js
@@ -1,16 +1,21 @@
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  if (!username || !password) {
-    console.log("Provide missing input");
-  }
-  //PARAMS AND JWT SECRET STRING
-  const token = jwt.sign({username,password},process.env.JWT.SECRET)
+    if (!username || !password) {
+        console.log("Provide missing input");
+    }
+
+    //PARAMS, JWT SECRET STRING AND EXPIRATION DATE FOR THE TOKEN
+    const token = jwt.sign({ username, password }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+    res.status(200).json({
+        message: `Inputs :${username}, ${password}`,
+        token: `${token}`,
+    });
 };
-
-
 
 module.exports = { login };
