7e34aef28b79b450bf84813092a73327289602ec
caesar <joaovictorcsatu@gmail.com>
2022-03-08 00:40:38 -0300

    Update

--- controllers.js
+++ controllers.js
@@ -6,11 +6,12 @@ const login = async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
+        res.status(201).send("Please provide the missing element");
         console.log("Provide missing input");
     }
 
     //PARAMS, JWT SECRET STRING AND EXPIRATION DATE FOR THE TOKEN
-    const token = jwt.sign({ username, password }, secret, {
+    const token = jwt.sign({ id, username }, secret, {
         expiresIn: "30d",
     });
     res.status(200).json({
